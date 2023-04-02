eb8c934478b74725df6c6b34bc9f2686b78f4c1a
petekostrytsya <petekostrytsya@gmail.com>
2023-04-02 09:31:36 +0300

    task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,46 @@
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+const createGalleryItemMarkup = ({ preview, original, description }) => {
+  return `
+    <li class="gallery__item">
+      <a
+        class="gallery__link"
+        href="${original}"
+      >
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+  `;
+};
+const galleryMarkup = galleryItems.reduce((acc, item) => {
+  return acc + createGalleryItemMarkup(item);
+}, "");
+
+galleryRef.insertAdjacentHTML("beforeend", galleryMarkup);
+
+// galleryRef.addEventListener("click", lightBox);
+
+// function lightBox(event) {
+//     event.preventDefault();
+//     const img = event.target.dataset.source;
+// }
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt', captionPosition: 'bottom', captionDelay: 500
+});
+
+// console.log(galleryItems);
